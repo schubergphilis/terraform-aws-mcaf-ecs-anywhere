2b21307e793f1bfaef450cf6d986f700194704b3
Marco Baan <mbaan@schubergphilis.com>
2022-03-14 11:16:41 +0100

    Output the IAM role id

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "cluster_id" {
   value       = aws_ecs_cluster.default.id
   description = "The ECS cluster ID"
 }
+
+output "ssm_role_id" {
+  value       = aws_ecs_cluster.default.id
+  description = "The IAM role ID used for the SSM activation"
+}
